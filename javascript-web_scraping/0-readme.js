934ee12b1b159f4e0ede4353fa09e60d52b73cad
OdinEM <e.oden@alustudent.com>
2023-10-08 14:04:34 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], function (err, data) {
-  err && console.log(err);
-  const content = data;
-  console.log(content.toString());
+const fs = require('fs');
+fs.readFile(process.argv[2], (err, resp) => {
+  if (err) {
+    throw err;
+  }
+  console.log(resp.toString());
 });
